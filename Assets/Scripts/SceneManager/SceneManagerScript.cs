c09de5e604337594844b5cbb4a0aa9e139cefebb
Dakota Molina <66569432+Dakorita@users.noreply.github.com>
2025-01-25 11:29:01 +0000

    menu basics

---
+++ Assets/Scripts/SceneManager/SceneManagerScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+
+    public void Play()
+    {
+
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+}
